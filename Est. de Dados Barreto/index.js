8bef61a312df93f48c543f10a2d126e74c7b3716
ghstN <98927555+ghstN@users.noreply.github.com>
2022-08-31 22:49:20 -0300

    Add files via upload

---
+++ Est. de Dados Barreto/index.js
@@ -0,0 +1,165 @@
+//Sétimo Exemplo
+var c = prompt ("Coloca em °C para transformar em F")
+eval(c)
+    var f = ((c*9)/5)+32
+    document.write("Você colocou em °C: " + c + ", que em F fica: " + f);
+    
+
+
+
+
+//Sexto Exemplo
+// for (i=1;i<=6;i++){
+//     document.write("<H" + i + ">Cabeçalho de nível " + i + "<H" + i + ">Acho que não")
+// }
+
+
+
+
+
+
+
+//Quinto Exemplo
+    // for (i=0;i<10;i=i+1){
+    //     document.write("<p>Linha"+i)
+    // }
+
+    // var i;
+    // for (i=0;i<10;i=i+1){
+    //     alert("A variavel i agora vale: " + i)
+    // }
+    // (
+    //     alert("A variável i terminou a repeticao valendo " + i )
+    // )
+        
+
+
+
+
+
+
+
+
+
+
+
+//Quarto Exemplo
+
+// n1 = prompt("Digite as suas três notas");
+// n1= eval (n1);
+
+//     if (n1==1)
+//         alert("Mais 1");
+//     else if
+//         (n1==2) 
+//         alert("Mais 2")
+//     else if 
+//         (n1==3) 
+//         alert("Mais 3")
+//     else if 
+//         (n1==4) 
+//         alert("Mais 4")    
+//     else if 
+//         (n1==5)
+//         alert("Número Certo");
+//     else if 
+//         (n1==6)
+//         alert("Menos 1");
+//     else if 
+//         (n1==7) 
+//         alert("Menos 2")
+//     else if 
+//         (n1==8)
+//         alert("Menos 3")
+//     else if 
+//         (n1==9)
+//         alert("Menos 4")
+//     else if
+//         (n1==10)
+//         alert("Menos 5")
+                        
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Terceiro Exemplo
+// var media, n1, n2, n3;
+
+// n1 = prompt("Digite as suas três notas");
+// n1= eval (n1) ;
+
+// n2 = prompt("Segunda");
+// n2= eval (n2) ;
+
+// n3 = prompt("Terceira");
+// n3= eval (n3) ;
+
+//     media = (n1 + n2 + n3)/3;
+
+//     if (media > 7){
+//     alert("Está acima da média do semestre");
+// }
+//     else{
+//         if(media==7){
+//         alert("Está na média do semestre");
+
+//     }
+//         else{
+//         alert("Você é ruim");
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+//Segundo Exemplo
+// var hora = new Date().getHours();
+//     if (hora < 12)
+//         alert("Bom Dia");
+//     if (hora >=  12 && hora < 18)
+//         alert("Boa Tarde");
+//     if (hora >= 18)
+//         alert("Boa Noite");
+
+
+
+
+
+
+
+
+
+
+
+// window.alert('alow')
+
+//Primeiro Exemplo
+// n1 = prompt("Digite um número", "")
+// n2 = prompt("Digite outro número", "")
+
+//     if (n1>n2){
+//         alert("O primeiro número é maior que o segundo")
+//     }
+//     else {
+//         alert ("O primeiro número é maior que o segundo")
+//     }
+    
+
+//     alert("FIM")
+    
